4dee7dc39b21b76d24ec7e4c2f013904919145bd
applecool <kmitharsha@gmail.com>
2015-07-09 10:25:06 -0400

    added unionterritories.js

--- union-territories/unionterritories.js
+++ union-territories/unionterritories.js
@@ -1,4 +1,4 @@
-[
+var union_territories =[
   {
     "name": "Andaman and Nicobar Islands",
     "abbreviation": "AN"
@@ -27,4 +27,16 @@
     "name": "Puducherry",
     "abbreviation": "PY"
   }
-]
+];
+
+var union_territories_hash = [
+  {
+  "AN":"Andaman and Nicobar Islands",
+  "CH": "Chandigarh",
+  "DN": "Dadra and Nagar Haveli",
+  "DD": "Daman and Diu",
+  "LD": "Lakshadweep",
+  "DL": "National Capital Territory of Delhi",
+  "PY": "Puducherry"
+}
+];
