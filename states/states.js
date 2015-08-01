e57ec9e10085c10c1953b21ee3427e3b5cdd7e6f
applecool <kmitharsha@gmail.com>
2015-08-01 11:08:40 -0400

    fixed formatting states.js

--- states/states.js
+++ states/states.js
@@ -1,124 +1,93 @@
-var states =[
-  {
-    "name": "Andhra Pradesh",
-    "abbreviation": "AP"
-  },
-  {
-    "name": "Arunachal Pradesh",
-    "abbreviation": "AR"
-  },
-  {
-    "name": "Assam",
-    "abbreviation": "AS"
-  },
-  {
-    "name": "Bihar",
-    "abbreviation": "BR"
-  },
-  {
-    "name": "Chhattisgarh",
-    "abbreviation": "CT"
-  },
-  {
-    "name": "Goa",
-    "abbreviation": "GA"
-  },
-  {
-    "name": "Gujarat",
-    "abbreviation": "GJ"
-  },
-  {
-    "name": "Haryana",
-    "abbreviation": "HR"
-  },
-  {
-    "name": "Himachal Pradesh",
-    "abbreviation": "HP"
-  },
-  {
-    "name": "Jammu and Kashmir",
-    "abbreviation": "JK"
-  },
-  {
-    "name": "Jharkhand",
-    "abbreviation": "JH"
-  },
-  {
-    "name": "Karnataka",
-    "abbreviation": "KA"
-  },
-  {
-    "name": "Kerala",
-    "abbreviation": "KL"
-  },
-  {
-    "name": "Madhya Pradesh",
-    "abbreviation": "MP"
-  },
-  {
-    "name": "Maharashtra",
-    "abbreviation": "MH"
-  },
-  {
-    "name": "Manipur",
-    "abbreviation": "MN"
-  },
-  {
-    "name": "Meghalaya",
-    "abbreviation": "ML"
-  },
-  {
-    "name": "Mizoram",
-    "abbreviation": "MZ"
-  },
-  {
-    "name": "Nagaland",
-    "abbreviation": "NL"
-  },
-  {
-    "name": "Odisha",
-    "abbreviation": "OR"
-  },
-  {
-    "name": "Punjab",
-    "abbreviation": "PB"
-  },
-  {
-    "name": "Rajasthan",
-    "abbreviation": "RJ"
-  },
-  {
-    "name": "Sikkim",
-    "abbreviation": "SK"
-  },
-  {
-    "name": "Tamil Nadu",
-    "abbreviation": "TN"
-  },
-  {
-    "name": "Telangana",
-    "abbreviation": "TG"
-  },
-  {
-    "name": "Tripura",
-    "abbreviation": "TR"
-  },
-  {
-    "name": "Uttar Pradesh",
-    "abbreviation": "UP"
-  },
-  {
-    "name": "Uttarakhand",
-    "abbreviation": "UT"
-  },
-  {
-    "name": "West Bengal",
-    "abbreviation": "WB"
-  }
-];
+var states = [{
+  "name": "Andhra Pradesh",
+  "abbreviation": "AP"
+}, {
+  "name": "Arunachal Pradesh",
+  "abbreviation": "AR"
+}, {
+  "name": "Assam",
+  "abbreviation": "AS"
+}, {
+  "name": "Bihar",
+  "abbreviation": "BR"
+}, {
+  "name": "Chhattisgarh",
+  "abbreviation": "CT"
+}, {
+  "name": "Goa",
+  "abbreviation": "GA"
+}, {
+  "name": "Gujarat",
+  "abbreviation": "GJ"
+}, {
+  "name": "Haryana",
+  "abbreviation": "HR"
+}, {
+  "name": "Himachal Pradesh",
+  "abbreviation": "HP"
+}, {
+  "name": "Jammu and Kashmir",
+  "abbreviation": "JK"
+}, {
+  "name": "Jharkhand",
+  "abbreviation": "JH"
+}, {
+  "name": "Karnataka",
+  "abbreviation": "KA"
+}, {
+  "name": "Kerala",
+  "abbreviation": "KL"
+}, {
+  "name": "Madhya Pradesh",
+  "abbreviation": "MP"
+}, {
+  "name": "Maharashtra",
+  "abbreviation": "MH"
+}, {
+  "name": "Manipur",
+  "abbreviation": "MN"
+}, {
+  "name": "Meghalaya",
+  "abbreviation": "ML"
+}, {
+  "name": "Mizoram",
+  "abbreviation": "MZ"
+}, {
+  "name": "Nagaland",
+  "abbreviation": "NL"
+}, {
+  "name": "Odisha",
+  "abbreviation": "OR"
+}, {
+  "name": "Punjab",
+  "abbreviation": "PB"
+}, {
+  "name": "Rajasthan",
+  "abbreviation": "RJ"
+}, {
+  "name": "Sikkim",
+  "abbreviation": "SK"
+}, {
+  "name": "Tamil Nadu",
+  "abbreviation": "TN"
+}, {
+  "name": "Telangana",
+  "abbreviation": "TG"
+}, {
+  "name": "Tripura",
+  "abbreviation": "TR"
+}, {
+  "name": "Uttar Pradesh",
+  "abbreviation": "UP"
+}, {
+  "name": "Uttarakhand",
+  "abbreviation": "UT"
+}, {
+  "name": "West Bengal",
+  "abbreviation": "WB"
+}];
 
-var states_hash =[
-  {
+var states_hash = [{
   "AP": "Andhra Pradesh",
   "AR": "Arunachal Pradesh",
   "AS": "Assam",
@@ -148,5 +117,4 @@ var states_hash =[
   "UP": "Uttar Pradesh",
   "UT": "Uttarakhand",
   "WB": "West Bengal"
-}
-];
+}];
