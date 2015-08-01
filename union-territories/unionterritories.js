f1c234a03648e61efb91c05e0aff0be54b3aa54f
applecool <kmitharsha@gmail.com>
2015-08-01 11:09:31 -0400

    fixed formatting union-territories.js

--- union-territories/unionterritories.js
+++ union-territories/unionterritories.js
@@ -1,42 +1,32 @@
-var union_territories =[
-  {
-    "name": "Andaman and Nicobar Islands",
-    "abbreviation": "AN"
-  },
-  {
-    "name": "Chandigarh",
-    "abbreviation": "CH"
-  },
-  {
-    "name": "Dadra and Nagar Haveli",
-    "abbreviation": "DN"
-  },
-  {
-    "name": "Daman and Diu",
-    "abbreviation": "DD"
-  },
-  {
-    "name": "Lakshadweep",
-    "abbreviation": "LD"
-  },
-  {
-    "name": "National Capital Territory of Delhi",
-    "abbreviation": "DL"
-  },
-  {
-    "name": "Puducherry",
-    "abbreviation": "PY"
-  }
-];
+var union_territories = [{
+  "name": "Andaman and Nicobar Islands",
+  "abbreviation": "AN"
+}, {
+  "name": "Chandigarh",
+  "abbreviation": "CH"
+}, {
+  "name": "Dadra and Nagar Haveli",
+  "abbreviation": "DN"
+}, {
+  "name": "Daman and Diu",
+  "abbreviation": "DD"
+}, {
+  "name": "Lakshadweep",
+  "abbreviation": "LD"
+}, {
+  "name": "National Capital Territory of Delhi",
+  "abbreviation": "DL"
+}, {
+  "name": "Puducherry",
+  "abbreviation": "PY"
+}];
 
-var union_territories_hash = [
-  {
-  "AN":"Andaman and Nicobar Islands",
+var union_territories_hash = [{
+  "AN": "Andaman and Nicobar Islands",
   "CH": "Chandigarh",
   "DN": "Dadra and Nagar Haveli",
   "DD": "Daman and Diu",
   "LD": "Lakshadweep",
   "DL": "National Capital Territory of Delhi",
   "PY": "Puducherry"
-}
-];
+}];
